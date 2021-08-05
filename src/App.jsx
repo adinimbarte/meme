e935da80fcd5b567d4e4d6d08723c33d010a3f67
Aditi Nimbarte <aditinimbarte341@gmail.com>
2021-08-05 19:13:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Templates from "./components/Templates";
+import Meme from "./components/Meme";
+function App() {
+  const [templates, setTemplates] = useState([]);
+  const [meme, setMeme] = useState(null);
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => {
+        setTemplates(data.data.memes);
+        console.log(data.data.memes);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <header>Meme Generator</header>
+      {meme === null ? <Templates templates={templates} setMeme={setMeme} /> : <Meme meme={meme} setMeme={setMeme}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Meme.jsx
@@ -0,0 +1,53 @@
+import React,{useState} from "react";
+import { saveAs } from 'file-saver';
+const Meme = ({ meme, setMeme }) => {
+    const [form, setForm] = useState({
+        template_id:meme.id,
+        username:"teasty",
+        password:"PPS$Asr52ttCfaF",
+        boxes:[],
+
+    });
+    const generateMeme =()=>{
+        let url = `https://api.imgflip.com/caption_image?template_id=${form.template_id}&username=${form.username}&password=${form.password}`;
+
+         // eslint-disable-next-line array-callback-return
+         form.boxes.map((box,index) => {
+          url+=`&boxes[${index}][text]=${box.text}` ;
+       });
+
+       fetch(url)
+       .then((res)=>res.json())
+       .then((data)=>{
+        if(data.success===true){
+            setMeme({...meme, url: data.data.url});
+            
+              }
+  
+              else{
+                  alert(`All boxes must be filled`);
+              }
+       }
+       );
+       console.log({generateMeme})
+        
+    }
+    return (<div className="meme">
+        <img src={meme.url} alt="" />
+        <div>
+            {[...Array(meme.box_count)].map((_, index) => (<input 
+           key={index} type="text" placeholder={`Enter Caption ${index + 1}`} onChange={(e)=>{
+                const newBoxes=form.boxes;
+                newBoxes[index]={text:e.target.value}
+                setForm({...form,boxes:newBoxes});
+            }} />))}
+        </div>
+        <div className="">
+            <button onClick={generateMeme}>Generate  Meme</button>
+            <button onClick={() => {setMeme(null)}}>Choose Template</button></div>
+            <button onClick={ ()=> {saveAs(meme.url, `${meme.name}`)}}>Download Meme</button>
+            </div>
+            
+            )
+}
+export default Meme;
